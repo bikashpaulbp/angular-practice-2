bd5c25b0f86bb6dea0ce5118a0e92ffe61ba491b
bikashpaulbp <bikashpaulbp92@gmail.com>
2024-06-22 22:00:24 +0600

    login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  email = new FormControl("", [Validators.required, Validators.email]);
 
+  password = new FormControl("", [Validators.minLength(6)]);
+
+  loginForm = new FormGroup({ email: this.email, password: this.password });
 }
